369aad8ac854790be1797e304321c96aa637e77b
Nahuel <99926121+nahufradev@users.noreply.github.com>
2024-12-18 21:54:31 -0300

    Add files via upload
    Conexion a la bd de manera local

---
+++ Models/DetallePedido.cs
@@ -0,0 +1,14 @@
+﻿namespace BackendProyectoRollingCode.Models
+{
+    public class DetallePedido
+    {
+        public int ID { get; set; }
+        public int PedidoID { get; set; }
+        public Pedido Pedido { get; set; }
+        public int ProductoID { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,12 @@
+﻿namespace BackendProyectoRollingCode.Models
+{
+    public class Producto
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public string ImagenUrl { get; set; }
+    }
+}
